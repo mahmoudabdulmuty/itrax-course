5702df324572bdf546710b09f6cf266a27889640
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-10-24 11:29:11 +0200

    *update*

--- JS/session-task/session-03/script.js
+++ JS/session-task/session-03/script.js
@@ -2,7 +2,8 @@ const h1 = document.createElement('h1');
 
 const calc = function () {
 	const tax = +document.getElementById('tax').value;
-	if (tax >= 10000) h1.textContent = (30 / 100) * tax;
+	if (!tax || tax < 0) h1.textContent = 'WRONG VALUE!!!!!!!!';
+	else if (tax >= 10000) h1.textContent = (30 / 100) * tax;
 	else if (tax >= 1000) h1.textContent = (20 / 100) * tax;
 	else h1.textContent = (10 / 100) * tax;
 };
