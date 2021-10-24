1a2277cf13f3844f5283d9e1d6327bc0339cc207
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-10-24 10:57:02 +0200

    *some updates*

--- JS/homework-tasks/task-03/script.js
+++ JS/homework-tasks/task-03/script.js
@@ -3,13 +3,15 @@ const h1 = document.createElement('h1');
 
 // submit Result function
 const submitResult = function () {
-	const grade = document.getElementById('grade');
+	const grade = +document.getElementById('grade').value;
+
 	let result = 'Result is ';
 
-	if (grade.value >= 85) result += 'A';
-	else if (grade.value >= 75) result += 'B';
-	else if (grade.value >= 65) result += 'C';
-	else if (grade.value >= 60) result += 'D';
+	if (grade > 100 || !grade) result = 'Wrong Entry';
+	else if (grade >= 85) result += 'A';
+	else if (grade >= 75) result += 'B';
+	else if (grade >= 65) result += 'C';
+	else if (grade >= 60) result += 'D';
 	else result += 'F';
 
 	h1.textContent = result;
