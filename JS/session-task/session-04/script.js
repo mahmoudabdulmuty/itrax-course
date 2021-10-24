069eba455d250482e060032a9e25667206c1cb4f
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-10-24 23:22:46 +0200

    session-4

---
+++ JS/session-task/session-04/script.js
@@ -0,0 +1,13 @@
+const names = ['mohamed', 'eslam', 'karam', 'ahmed', 'radwa', 'sara'];
+
+let i = 0;
+while (i < names.length) {
+	const name = names[i];
+	console.log(name);
+	i++;
+}
+
+for (let i = 1; i <= 13; i++) {
+	if (i === 7) console.log('Mahmoud');
+	else console.log(i);
+}
