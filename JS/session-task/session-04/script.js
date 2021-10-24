7b003252766f787b9f8289070c572f9b3f392881
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-10-24 23:54:26 +0200

    update session-04

--- JS/session-task/session-04/script.js
+++ JS/session-task/session-04/script.js
@@ -11,3 +11,28 @@ for (let i = 1; i <= 13; i++) {
 	if (i === 7) console.log('Mahmoud');
 	else console.log(i);
 }
+
+const numbers = [20, 50, 1, 65, 5, 100, 567, 7];
+
+const getMax = function (arr) {
+	let max = arr[0];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	return max;
+};
+
+const getMin = function (arr) {
+	let min = arr[0];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	return min;
+};
+
+console.log(getMax(numbers));
+console.log(getMin(numbers));
