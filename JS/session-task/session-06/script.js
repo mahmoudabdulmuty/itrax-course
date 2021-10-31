7ef67eb1f2f88e6e7a1db74e6be4ba7d0030759b
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-10-31 22:42:36 +0200

    session-06

---
+++ JS/session-task/session-06/script.js
@@ -0,0 +1,22 @@
+// Solution 1
+// const img1 = document.getElementById('img-1');
+// const img2 = document.getElementById('img-2');
+// const img3 = document.getElementById('img-3');
+// const defaultImg = document.getElementById('default');
+
+// img1.onclick = function () {
+// 	defaultImg.src = './img/img-1.jpg';
+// };
+
+// img2.onclick = function () {
+// 	defaultImg.src = './img/img-2.jpg';
+// };
+
+// img3.onclick = function () {
+// 	defaultImg.src = './img/img-3.jpeg';
+// };
+
+// Solution 2
+// function changeSrc(img) {
+// 	document.getElementById('default').src = img;
+// }
