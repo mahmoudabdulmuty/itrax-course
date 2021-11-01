77e93a56d24f348397090107a8b96a85d4d76f7a
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-11-01 12:44:41 +0200

    update

--- JS/session-task/session-06/script.js
+++ JS/session-task/session-06/script.js
@@ -1,20 +1,20 @@
 // task01 -- Solution 1
-const img1 = document.getElementById('img-1');
-const img2 = document.getElementById('img-2');
-const img3 = document.getElementById('img-3');
-const defaultImg = document.getElementById('default');
+// const img1 = document.getElementById('img-1');
+// const img2 = document.getElementById('img-2');
+// const img3 = document.getElementById('img-3');
+// const defaultImg = document.getElementById('default');
 
-img1.onclick = function () {
-	defaultImg.src = './img/img-1.jpg';
-};
+// img1.onclick = function () {
+// 	defaultImg.src = './img/img-1.jpg';
+// };
 
-img2.onclick = function () {
-	defaultImg.src = './img/img-2.jpg';
-};
+// img2.onclick = function () {
+// 	defaultImg.src = './img/img-2.jpg';
+// };
 
-img3.onclick = function () {
-	defaultImg.src = './img/img-3.jpeg';
-};
+// img3.onclick = function () {
+// 	defaultImg.src = './img/img-3.jpeg';
+// };
 
 // task01 -- Solution 2
 function changeSrc(img) {
